d0999b399933105a4941418a0f8dafb362d4c5fe
quincynash <73974411+quincynash@users.noreply.github.com>
2021-01-02 12:54:05 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,50 +3,99 @@ let highscoreButtons = [];
 let font;
 let state = "home";
 let scores;
-let title, options, play, shipColor, highscores, upgrades, optionsBack, highscoreBack;
+let title, options, play, shipColor, highscores, upgrades, optionsBack, highscoreBack, xhr;
 
 function preload() {
   font = loadFont("space-obsessed.ttf")
   scores = loadJSON("scores.json")
 }
 
+function get_scores(callback) {
+  // High Score Data
+  let file = "scores.json";
+
+  // Fetch High Score Data
+  fetch(file, {cache: 'no-cache'})
+    .then(function(response) {
+        //  If the response isn't OK
+      if (response.status !== 200) {
+        Errors.innerHTML = response.status;
+      }
+      // If the response is OK
+      response.json().then(function(data) {
+        let scores = JSON.stringify(data);
+        console.log(scores);
+        callback(scores);
+      });
+    })
+    // If there is an error
+    .catch(function(err) {
+      console.log(err)
+    });
+}
+
+function submitHighscore(name, score) {
+  myform = document.getElementById("my-form")
+  myname = document.getElementsByName("player_name")[0]
+  myscore = document.getElementsByName("player_score")[0]
+  myname.value = name
+  myscore.value = score
+    
+  formData = new FormData(myform)
+  fetch ("asteroids.php", {
+    method: "post",
+    body: formData
+  })
+  .then (function (response){
+    return response.text();
+  })
+  .then(function(text) {
+    console.log(text);
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
+}
+
 function setup() {
   createCanvas(0, 0)
-  
+    
+  submitHighscore("Misha Harleman", 3)
+    
   title = new Button("Asteroids", 0, 0, 0, false)
-  
+
   options = new Button("Options", 0, 0, 0, false)
   options.onclick(showOptions)
-  
+
   play = new Button("Play Game", 0, 0, 0, false)
   play.onclick(playGame)
-  
+
   shipColor = new Button("Change Color", 0, 0, 0, false)
   shipColor.hide()
   shipColor.onclick()
-  
+
   highscores = new Button("Highscores", 0, 0, 0, false)
   highscores.hide()
   highscores.onclick(showHighscores)
-  
+
   upgrades = new Button("Upgrades", 0, 0, 0, false)
   upgrades.hide()
   upgrades.onclick()
-  
+
   optionsBack = new Button("Back", 0, 0, 0, false, false, false)
   optionsBack.hide()
   optionsBack.onclick(homeScreen)
-  
+
   highscoreBack = new Button("Back", 0, 0, 0, false, false, false)
   highscoreBack.hide()
   highscoreBack.onclick(showOptions)
-  
+
   for (var i = 0; i < Object.keys(scores).length; i++) {
     button = new Button(scores[i].name + ": " + scores[i].score, 0, 0, 0, false)
     button.hide()
     highscoreButtons.push(button)
   }
-  
+
   resize()
 }
 
@@ -108,16 +157,24 @@ function resize() {
   for (var i = 0; i < highscoreButtons.length; i++) {
     highscoreButtons[i].change(width/2, map(i, 0, highscoreButtons.length, 50, height), width/30)
   }
-} 
+}
 
 function windowResized() {
   resize()
 }
-  
+
 function draw() {
   background(0)
   for (var button of buttons) {
     button.update()
     button.draw()
+  }
+  if (state == "highscores" && frameCount % 60 == 0) {
+    get_scores(function(scores) {
+      let object = JSON.parse(scores);
+      for (var i = 0; i < highscoreButtons.length; i++) {
+        highscoreButtons[i].string = object[i].name + ": " + str(object[i].score)
+      }
+    })
   }
 }
