f4f16a5bb2835ecb9d1a4ea7e0ab47408b8d3cc1
quincynash <73974411+quincynash@users.noreply.github.com>
2021-01-02 12:52:12 -0800

    Update asteroids.php

--- asteroids.php
+++ asteroids.php
@@ -1,1 +1,18 @@
+<?php
+  $player_name = filter_var($_POST["player_name"], FILTER_SANITIZE_STRING);
+  $player_score = (int)$_POST["player_score"];
+  $player_array = array("name" => $player_name, "score" => $player_score);
 
+  $highscoreJson = file_get_contents("scores.json");
+  $highscores = json_decode($highscoreJson, true);
+
+  for ($i = 0; $i < count($highscores) - 1; $i++) {
+    if ($player_score > $highscores[$i]["score"]) {
+      $highscores[$i] = $player_array;
+      break;
+    }
+  }
+
+  $jsonscores = json_encode($highscores);
+  file_put_contents('scores.json', $jsonscores);
+?>
